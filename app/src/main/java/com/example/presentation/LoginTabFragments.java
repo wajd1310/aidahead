2e0f22cd40f8e12dd653c0a225c923f9605c0108
jameel619 <jameelsaeb05@gmail.com>
2022-07-22 18:05:11 +0300

    home page, library w/o java

--- app/src/main/java/com/example/presentation/LoginTabFragments.java
+++ app/src/main/java/com/example/presentation/LoginTabFragments.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -53,4 +54,5 @@ public class LoginTabFragments extends Fragment {
         mAuth=FirebaseAuth.getInstance();
         return root;
     }
+
 }
