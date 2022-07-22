5dab61dcc3f0ad740f9c6862e142ac774f07dadc
wajd1310 <wajdhallak@gmail.com>
2022-07-22 13:38:11 +0300

    wajd's 2nd commit

--- app/src/main/java/com/example/presentation/LoginTabFragments.java
+++ app/src/main/java/com/example/presentation/LoginTabFragments.java
@@ -2,6 +2,7 @@ package com.example.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,21 +20,23 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginTabFragments extends Fragment {
-    float v=0;
+    float v = 0;
     Button login;
-    EditText email,pass;
-    TextView forgetPass,signup;
+    EditText email, pass;
     FirebaseAuth mAuth;
+    TextView forgetPass, signup;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment,container,false);
-        email =root.findViewById(R.id.email);
-        pass=root.findViewById(R.id.pass);
-        forgetPass = root.findViewById(R.id.pass);
-        signup=root.findViewById(R.id.signup);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
+        email = root.findViewById(R.id.email);
+        pass = root.findViewById(R.id.pass);
+        signup = root.findViewById(R.id.signup);
         login = root.findViewById(R.id.button);
+        mAuth=FirebaseAuth.getInstance();
         email.setTranslationX(800);
         pass.setTranslationX(800);
         forgetPass.setTranslationX(800);
@@ -44,13 +47,43 @@ public class LoginTabFragments extends Fragment {
         signup.setAlpha(v);
         forgetPass.setAlpha(v);
         login.setAlpha(v);
-        email.animate().translationX(0).alpha(1).setDuration (800).setStartDelay(300).start();
-        pass.animate().translationX(0).alpha (1).setDuration (800).setStartDelay(500).start();
-        forgetPass.animate().translationX(0).alpha (1).setDuration (800).setStartDelay(500).start();
-        login.animate().translationX(0).alpha(1).setDuration (800).setStartDelay (700).start();
-        signup.animate().translationX(0).alpha(1).setDuration (800).setStartDelay (700).start();
+        email.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(300).start();
+        pass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
+        forgetPass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
+        login.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(700).start();
+        signup.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(700).start();
         signup.setOnClickListener((view -> startActivity(new Intent(getActivity(), SignupActiv.class))));
-        mAuth=FirebaseAuth.getInstance();
+        login.setOnClickListener(view->{
+            loginUser();
+            startActivity(new Intent(getActivity(), mainPage.class));
+        });
+
+
         return root;
     }
+    private void loginUser() {
+        String email1 = email.getText().toString();
+        String password = pass.getText().toString();
+        if (TextUtils.isEmpty(email1)) {
+            email.setError("Email cannot be empty");
+            email.requestFocus();
+        } else if (TextUtils.isEmpty(password)) {
+            pass.setError("Password cannot be empty");
+            pass.requestFocus();
+        }else {
+            mAuth.signInWithEmailAndPassword(email1, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getActivity(), "User logged in successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getActivity(), mainPage.class));
+                    } else {
+                        Toast.makeText(getActivity(), "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 }
+
+
---
+++ app/src/main/java/com/example/presentation/mainPage.java
@@ -0,0 +1,6 @@
+package com.example.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainPage extends AppCompatActivity {
+}
