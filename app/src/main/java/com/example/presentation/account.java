c52e198354f8314bfe0d7423aca4e0b23ac7e323
wajd1310 <wajdhallak@gmail.com>
2022-07-27 17:34:10 +0300

    quizzes

--- app/src/main/java/com/example/presentation/account.java
+++ app/src/main/java/com/example/presentation/account.java
@@ -2,13 +2,9 @@ package com.example.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,22 +12,35 @@ public class account extends AppCompatActivity{
     private ImageButton leaderboardBtn;
     private ImageButton homeBtn;
     private ImageButton libraryBtn;
+    private Button Quiz;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account);
         leaderboardBtn=findViewById(R.id.leaderboard);
+        Quiz=findViewById(R.id.button2);
         homeBtn=findViewById(R.id.home);
         libraryBtn=findViewById(R.id.library);
         leaderboardBtn.setOnClickListener(view->{
             startActivity(new Intent(account.this, Leaderboard.class));
+            overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+        });
+        Quiz.setOnClickListener(view->{
+            startActivity(new Intent(account.this, QuizActivity.class));
+            overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
         });
         homeBtn.setOnClickListener(view->{
             startActivity(new Intent(account.this, mainPage.class));
+            overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
         });
         libraryBtn.setOnClickListener(view->{
             startActivity(new Intent(account.this, Library.class));
+            overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
         });
 
+        libraryBtn.setOnClickListener(view->{
+            startActivity(new Intent(account.this, Library.class));
+            overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+        });
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
